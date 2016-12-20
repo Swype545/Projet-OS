727aca5d835ea47c6531eb62a4d248238059e7f3
Swype545 <13069@ecam.be>
2016-12-20 21:42:44 +0100

    Hadrien cmd update

--- ProjectOS/cmd/myuptime.c
+++ ProjectOS/cmd/myuptime.c
@@ -1,73 +1,66 @@
+
+#include <utmpx.h>
+#include <unistd.h>
 #include <getopt.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 #include <sys/sysinfo.h>
 #include <time.h>
-#include <utmpx.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[])
 {
-    //variables
+    //variables flag
     int pflag = 0;
     int sflag = 0;
 
-    
-    char *since;
-    //variables since
-    int lenSince = 20;
-
-    char strSince[lenSince];
-   
-    time_t nowSince;
-
-    time_t startSince;
   
-    struct tm struStart;
-
-    struct sysinfo struInfo;
     //variables uptime
-
-    int str_len2 = 512;
-    //char *strTime;
-    char *strTime;
-
-    char *strUptime;
-    //char *str_res2;
-    char *uptimeResult;
+    long secondes, minutes, hours, days, weeks, years;
     long valeurUptime;
-    //struct sysinfo struInfo;
     long minute = 60;
     long hour = minute * 60;
     long day = hour * 24;
     long week = 7 * day;
     long year = 365 * day;
-    long secondes, minutes, hours, days, weeks, years;
-
+    int lenUpime = 1000;
+    char *strTime;
+    char *strUptime;
+    char *uptimeResult;
+    
     //variable nowTime
     char *nowTime;
     int lenNow = 100;
     char strNow[lenNow];
-    time_t now;
     struct tm struNow;
-////-> check
+    time_t now;
+    
+
     //variables nb user
-    int nbUsers = 0;
     struct utmpx *user_info;
-
+    int nbUsers = 0;
+    
     //variables getload
-
-    int lenload = 512;
+    int lenload = 1000;
     char *strload;
     char *load;
-    //struct sysinfo struInfo;
+
+    //variables since
+    int lenSince = 100;
+    char *since;
+    char strSince[lenSince]; 
+    struct tm struStart;
+    struct sysinfo struInfo; 
+    time_t nowSince;
+    time_t startSince;
 
     //gestion d'erreurs
     char *prog_name;
     prog_name = argv[0];
-    //flags ps
 
+    //flags ps
     int c;
     struct option options[] = 
     {
@@ -75,9 +68,7 @@ int main(int argc, char *argv[])
         {"since", no_argument, NULL, 's'},
         {0, 0, 0, 0}
     };
-
-
-    while ((c = getopt_long(argc, argv,"ps", options, NULL)) != -1) {
+      while ((c = getopt_long(argc, argv,"ps", options, NULL)) != -1) {
         switch (c) {
             case 'p':
                 pflag = 1;
@@ -89,199 +80,174 @@ int main(int argc, char *argv[])
                 abort();
         }
     }
-    //Retourner since
-    //a mettre a la fin
-    if (sflag) {
-
-
-    if (sysinfo(&struInfo) == -1)
-        return EXIT_FAILURE;
-
-    nowSince = time(NULL);
-    if (nowSince == -1)
-        return EXIT_FAILURE;
-
-    startSince = nowSince - struInfo.uptime;
-
-    if (localtime_r(&startSince, &struStart) == NULL)
-        return EXIT_FAILURE;
-
-    if (strftime(strSince, lenSince, "%Y-%m-%d %H:%M:%S", &struStart) < 0)
-        return EXIT_FAILURE;
-
-
-    since = strdup(strSince);//////////??????
-    if (since == NULL) {
-            perror(prog_name);
-            exit(EXIT_FAILURE);
-        }
-    printf("%s\n", since);
-    free(since);
-    exit(EXIT_SUCCESS);
-    }
-////////////////////////////////////////////////////////
-
-
-    /*int str_len2 = 512;
-    char *strTime;
-    char *strUptime;
-    char *uptimeResult;
-    long uptime;
-    struct sysinfo struInfo;
-    long minute = 60;
-    long hour = minute * 60;
-    long day = hour * 24;
-    long week = 7 * day;
-    long year = 365 * day;
-    long secondes, minutes, hours, days, weeks, years;*/
-
-    strUptime = calloc(str_len2, sizeof(char));
+    //Temps actuel & temps-P
+    //
+    //on va allouer de la place pour stocker notre donnée de temps
+    strUptime = calloc(lenUpime, sizeof(char));
     if (strUptime == NULL)
         return EXIT_FAILURE;
 
-    strTime = calloc(str_len2, sizeof(char));
+    strTime = calloc(lenUpime, sizeof(char));
     if (strTime == NULL)
         return EXIT_FAILURE;
 
     if (sysinfo(&struInfo) == -1)
         return EXIT_FAILURE;
 
-    valeurUptime = struInfo.uptime;
-
     years = valeurUptime / year;
     weeks = (valeurUptime % year) / week;
-    // les jours sont différents avec le pflag
     hours = (valeurUptime % day) / hour;
     minutes = (valeurUptime % hour) / minute;
-
+    valeurUptime = struInfo.uptime;
+    // les semeines sont différents avec le pflag
     if (pflag) {
         days = (valeurUptime % week) / day;
-        // years
         if (years) {
+           // on va concatener la chaine qu'on veut afficher avec les bonnes infos de temps
             sprintf(strUptime, "%ld years,", years);
             strcat(strTime, strUptime);
         }
-        // Weeks
         if (weeks) {
             sprintf(strUptime, " %ld weeks,", weeks);
             strcat(strTime, strUptime);
         }
-        // days
         if (days) {
             sprintf(strUptime, " %ld days,", days);
             strcat(strTime, strUptime);
         }
-        // Hours
         if (hours) {
             sprintf(strUptime, " %ld hours,", hours);
             strcat(strTime, strUptime);
         }
-        // Minutes
+        //on ne prends que les 2 premiers digit du long retourner par minute
         if (minutes) {
             sprintf(strUptime, " %02ld minutes", minutes);
             strcat(strTime, strUptime);
         }
     } else {
         days = (valeurUptime % year) / day;
-        // years
         if (years) {
             sprintf(strUptime, "%ld years, ", years);
             strcat(strTime, strUptime);
         }
-        // days
         if (days) {
             sprintf(strUptime, "%ld days, ", days);
             strcat(strTime, strUptime);
         }
-        // Hours
         if (hours) {
             sprintf(strUptime, "%ld:", hours);
             strcat(strTime, strUptime);
         }
-        // Minutes
         if (minutes) {
             if (hours) 
                 sprintf(strUptime, "%02ld", minutes);
             else
                 sprintf(strUptime, "%02ld minutes", minutes);
 
+            //on concatene le tout dans strTime
             strcat(strTime, strUptime);
         }
     }
 
     free(strUptime);
+    //on copie le résultat dans uptimeResult
     uptimeResult = strdup(strTime);
     free(strTime);
 
+    //on va vérifier que la str crée n'est pas vide
     if (uptimeResult == NULL) {
         perror(prog_name);
         exit(EXIT_FAILURE);
     }
-    // a mettre a la fin
+    //Si on a pflag on va afficher la date bien formatée et on fini le programme ici en libérant la mémoire
     if (pflag) {
-        printf("%s\n", uptimeResult);
-        free(uptimeResult);
+        printf("%s\n", uptimeResult);  
         exit(EXIT_SUCCESS);
+        free(uptimeResult);
     }
-    //////////////////////////////////////
-
-    /*int lenNow = 100;
-    char strNow[lenNow];
-    time_t now_raw;
-    struct tm struNow;*/
-
+    //temps actuel
+    //
+    //on prends le temps actuel
     now = time(NULL);
     if (now == -1)
         return EXIT_FAILURE;
-
+    //on stock notre temps dans une tructure tm
     if (localtime_r(&now, &struNow) == NULL)
         return EXIT_FAILURE;
-
+    //on choisi les éléments de la structure qu'on veut grace aux argument et on les inscrit sous forme de chaine de caractère
     if (strftime(strNow, lenNow, "%H:%M:%S", &struNow) < 0)
         return EXIT_FAILURE;
-
+    //on fait une copie dans nowTime et on vrifie la valeur retournée en cas d'erreur
     nowTime = strdup(strNow);
     if (nowTime == NULL) {
         perror(prog_name);
         exit(EXIT_FAILURE);
     }
-//////////////////////////////////////
-
-
+    //nbUser
+    //
+    //on va compter le nombre d'user (meme que poue la commande "Who")
+    //on set le curseur au début de utempx
     setutxent();
-
-   // nbUsers = 0;
+    //on va vérifier si la structure n'est pas vide, si c'est vide ca renvoit NULL
     while ((user_info = getutxent()) != NULL) {
+        //on filtre pour avoir les process normaux
         if (user_info->ut_type == 7)
+            //on augmente le nombre d'user si on a passé le filtre 
             nbUsers++;
     }
-    // nbUsers = myuptime_count_connected_users();
-    if (nbUsers == -1) {
-        perror(prog_name);
-        exit(EXIT_FAILURE);
-    }
- ///////////////////////////////////
-
+    //Load
+    //on va afficher la charge a 3 périodes de temps(1-10-15min)
+    //
+    //on alloue la place en mémoire
     strload = calloc(lenload, sizeof(char));
     if (strload == NULL)
         return EXIT_FAILURE;
-
+    //on vérifie que la struct envoyée est bien une de type sysinfo
     if (sysinfo(&struInfo) == -1)
         return EXIT_FAILURE;
-
+    //on va créer la string qu'on veut afficher en la retenant dans la chaine de caractère préalablement allouée "strload"
+    //on affiche 2 digit apres la virgule  des float retournées par la structure, on les divise pour avoir un résultat "user friendly"
     sprintf(strload,"%.2f, %.2f, %.2f", struInfo.loads[0] / (float)(1 << SI_LOAD_SHIFT),struInfo.loads[1] / (float)(1 << SI_LOAD_SHIFT),struInfo.loads[2] / (float)(1 << SI_LOAD_SHIFT));
-
+    //on clone la string dans la variable load
     load = strdup(strload);
-
+    //on libère la mémoire
     free(strload);
-
+    //on fait la vérification
     if (load == NULL) {
         perror(prog_name);
         exit(EXIT_FAILURE);
     }
-
+     //Since
+    //
+    //si on a le flag since.
+    if (sflag) {
+    //on lui fournit une structure sysinfo
+    if (sysinfo(&struInfo) == -1)
+        return EXIT_FAILURE;
+    //on prends le temps actuel
+    nowSince = time(NULL);
+    //on fait la différence entre la veleur stocké dans sysinfo de boot et le temps actuel
+    startSince = nowSince - struInfo.uptime;
+    //on stock le temps dans une structure tm de laquel on va pouvoir récuperer les informations du temps
+    if (localtime_r(&startSince, &struStart) == NULL)
+        return EXIT_FAILURE;
+    //on place dans un string selon le format %Y-%m-%d %H:%M:%S"
+    if (strftime(strSince, lenSince, "%Y-%m-%d %H:%M:%S", &struStart) < 0)
+        return EXIT_FAILURE;
+    //on fait une copie en mémoire cache de strSince dans la variable since 
+    since = strdup(strSince);
+    if (since == NULL) {
+            perror(prog_name);
+            exit(EXIT_FAILURE);
+        }
+    //Si il n'y a pas d'erreur on print le sice 
+    printf("%s\n", since);
+    free(since);
+    exit(EXIT_SUCCESS);
+    }
 
     //affiche les résultats 
+    //
     printf("%s ", nowTime);
 
     printf("up %s, ", uptimeResult);
@@ -294,10 +260,6 @@ int main(int argc, char *argv[])
         printf("%d user, ", nbUsers);
     }
     printf("load average: %s\n", load);
-
-    free(nowTime);
-    free(uptimeResult);
-    free(load);
 
     return EXIT_SUCCESS;
 }
--- ProjectOS/cmd/mywho.c
+++ ProjectOS/cmd/mywho.c
@@ -1,61 +1,57 @@
-#include <ctype.h>
-#include <errno.h>
-#include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/syscall.h>
+
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 #include <utmpx.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
+#include <getopt.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
-//Main
 int main(int argc, char *argv[]) {
-	
-	
-	//flags s q b H 
-	int fcount = 0;
-	int fboot = 0;
+	//flags s q H 
 	int fhead = 0;
 	int fsmall = 0;
-
-	//boot time ==2 or user mode ==7
-	int mode=7;
+	int fcount = 0;
 	
-	// nombre d'utilisateurs
-	int nbusers = 0;
-
 	//tester 100 ou plus si ca marche pas
-	int ar_len = 100;
-	char time[ar_len];
+	int arlen = 100;
+	char strTime[arlen];
+
 	//structure time type tm		
 	struct tm strutime;
 
 	//Structure utmpx
+	//on va utiliser cette structure pour récuperer toutes les informations à chaque ittération pour un utilisateur.
 	struct utmpx *strutmpx;
 
-	//For error on cmd 
+	// nombre d'utilisateurs pour le -q
+	int nbusers = 0;
+
+
+	//Gestion d'erreurs on renvoit le nom du programme
 	char *prog_name;
 	prog_name = argv[0];
 
-	//flags bqsH
+	//on va aller lire dans un struct on remet le curseur a zéro dans utmpx
+	setutxent();
+
+	//détermination des flags
 	int c;
-	
+
 	struct option options[] =
 	{
-			{"boot", no_argument, NULL, 'b'},
 			{"count", no_argument, NULL, 'q'},
 			{"name", no_argument, NULL, 's'},
 			{"heading", no_argument, NULL, 'H'},
 			{0,0,0,0}
 	};
-	while ((c = getopt_long(argc, argv, "bqsH",options, NULL)) != -1){
+	while ((c = getopt_long(argc, argv, "qsH",options, NULL)) != -1){
 		switch(c){
-				case 'b':
-					fboot = 1;
-					break;
+				
 				case 'q':
 					fcount =  1;
 					break;
@@ -70,63 +66,43 @@ int main(int argc, char *argv[]) {
 					abort();
 		}
 	}
-	//fonction Who
-	if (fboot)
-	{
-		mode=2;
-		//on change le type d'user
-	}
-
-	setutxent();
-	//on va aller lire dans un struct on remet le curseur a zéro dans utmpx
-
+	
+	//on va inscrire dans la structure utmpx, tant qu'il y aura des user le get ne sera pas nul on aura donc les info via utmpx pour chaque iutilisateur
 	while ((strutmpx = getutxent()) != NULL) {
-	//Pint Normal process
-		if (strutmpx->ut_type == 7) {
-			//who -H
-			if (fhead == 1 && nbusers == 0 && fboot !=1 )
-				{
-					printf("NAME\tLINE\tTIME\t \n");	
-					}
-				//who 
-				if (fcount != 1) {
 			
-					//afficher nom des user
-					printf("%s\t", strutmpx->ut_user);
-					//afficher line user
-					printf("%s\t", strutmpx->ut_line);
-					//getTime
-					if (localtime_r((time_t *)&strutmpx->ut_tv.tv_sec, &strutime) == NULL) {
-						perror(prog_name);
-						return EXIT_FAILURE;
-					}
-					//afficher le temps 
-					if (strftime(time,ar_len, "%Y-%m-%d %H:%M",&strutime)<0){
-						perror(prog_name);
-						return EXIT_FAILURE;
-					}
-					printf("%s\t", time);
-					//printf("(%s)\t", strutmpx->ut_host);
-					printf("\n");
-				}
-				//on va incrémenter le nbuser a chaque ittération, quaque fois que user process est de type normal
-				nbusers++;
-		}
-	}
+			//on va filtrer les valeur d'utmpxqui auront le label 7 (pour normal process)
+        	if (strutmpx->ut_type == 7) {
 
-   	if(fboot)
-    {
-    	if (strutmpx->ut_type == mode) 
-    	{
-    		if (strftime(time,ar_len, "%Y-%m-%d %H:%M",&strutime)<0)
-    		{
-				perror(prog_name);
-				return EXIT_FAILURE;
-			}
-		}
-
-    	printf("time since reboot: %s\t", time);
-    }
+				//on va vérifier si le flag h est coché et qu'on est a la première ittération on va afficher un header
+				if (fhead == 1 && nbusers == 0 )
+					{
+                   		printf("NAME\tLINE\tTIME\t  \n");	
+                    	}
+					// si on ne doit pas compter le nbre d'users
+            		if (fcount != 1) {
+                		//afficher nom des user
+                		printf("%s\t", strutmpx->ut_user);
+						//affiche le nom du device
+                		printf("%s\t", strutmpx->ut_line);
+						//getTime
+                		if (localtime_r((time_t *)&strutmpx->ut_tv.tv_sec, &strutime) == NULL) {
+                    		perror(prog_name);
+                    		return EXIT_FAILURE;
+                		}
+						//afficher le temps 
+						if (strftime(strTime,arlen, "%Y-%m-%d %H:%M",&strutime)<0){
+							perror(prog_name);
+							return EXIT_FAILURE;
+						}
+                		printf("%s\t", strTime);
+                		printf("(%s)\t", strutmpx->ut_host);
+                		printf("\n");
+            		}
+            		//on va incrémenter le nbuser a chaque ittération si on a le flag de comptage
+            		if (fcount)
+            			nbusers++;
+        	}
+    	}
 	//on compte le nombre d'utilisateurs
 	if (fcount)
 	{
@@ -135,7 +111,12 @@ int main(int argc, char *argv[]) {
         	perror(prog_name);
         	exit(EXIT_FAILURE);
     	}
-        printf("\n#users=%d\n", nbusers);
+    	//on l'affiche en tenant compte du nombre d'utilisateurs
+    	if (nbusers >1)
+    	{
+    		printf("\n #user=%d \n", nbusers);
+    	}
+        printf("\n #users=%d \n", nbusers);
 	}
 
 	endutxent();
